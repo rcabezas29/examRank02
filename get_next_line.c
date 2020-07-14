a18f894f5b7656ce541b45f8bf761de180622400
Raul Cabezas gonzalez <rcabezas@c1r10s1.42madrid.com>
2020-07-14 13:03:44 +0200

    gnl sin leaks

--- get_next_line.c
+++ get_next_line.c
@@ -1,36 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rcabezas <rcabezas@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/07/14 10:59:15 by rcabezas          #+#    #+#             */
+/*   Updated: 2020/07/14 13:03:08 by rcabezas         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "get_next_line.h"
 
-int     ft_strlen(char *str)
+int ft_strlen(char *str)
 {
-    int     i;
+    int i;
+
     i = 0;
     while (str[i] != '\0')
         i++;
-    return(i);
+    return (i);
 }
 
-char    *ft_strdup(char *str)
+char *ft_strdup(char *s)
 {
-    int     i;
-    char    *dest;
-    if (!(dest = malloc(ft_strlen(str) + 1)))
-        return(0);
+    int i;
+    char *dest;
+    char *tmp;
+
     i = 0;
-    while (str[i] != '\0')
+    if (!(dest = malloc(ft_strlen(s) + 1)))
+        return (0);
+    while (s[i] != '\0')
     {
-        dest[i] = str[i];
+        dest[i] = s[i];
         i++;
     }
     dest[i] = '\0';
-    return(dest);
+    tmp = dest;
+    free(dest);
+    return (tmp);
 }
 
-char    *ft_strjoin(char *str, char *c)
+char *ft_strjoin(char *str, char *c)
 {
-    int     i;
-    char    *dest;
-    if(!(dest = malloc(ft_strlen(str) + ft_strlen(c) + 1)))
-        return(0);
+    int i;
+    char *dest;
+    char *tmp;
+    if (!(dest = malloc(ft_strlen(str) + ft_strlen(c) + 1)))
+        return (0);
     i = 0;
     while (str[i] != '\0')
     {
@@ -39,16 +57,18 @@ char    *ft_strjoin(char *str, char *c)
     }
     dest[i] = c[0];
     dest[i + 1] = '\0';
-    return(dest);
+    tmp = dest;
+    free(dest);
+    return (tmp);
 }
 
-int     get_next_line(char **line)
+int get_next_line(char **line)
 {
-    char    buf[1];
-    int     ret;
+    char buf[1];
+    int ret;
 
     if (line == '\0' || !(*line = ft_strdup("")))
-        return(-1);
+        return (-1);
     while ((ret = read(0, buf, 1)) > 0)
     {
         if (buf[0] == '\n' || buf[0] == '\0')
@@ -59,23 +79,20 @@ int     get_next_line(char **line)
         *line = ft_strjoin(*line, buf);
     }
     buf[0] = '\0';
-    return(ret);
+    return (ret);
 }
-/*
-int     main(void)
+
+/*int     main(void)
 {
     int        r;
-    int        fd;
     char    *line;
     line = NULL;
     while ((r = get_next_line(&line)) > 0)
     {
         printf("%s\n", line);
-        free(line);
         line = NULL;
     }
     printf("%s", line);
-    free(line);
     line = NULL;
     system ("leaks a.out");
 }*/
